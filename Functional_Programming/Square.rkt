2d12de599ba63af58e4e9d8ccffd35bd58c38695
niallmc14 <niallmc14@gmail.com>
2019-01-14 17:11:39 +0000

    Add files via upload

---
+++ Functional_Programming/Square.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname Square) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+(define square 
+  (lambda (n)
+    (* n n)
+    )
+  )
