ac5b298fa62f2df88a0b38c722e4de7bfdade2db
niallmc14 <niallmc14@gmail.com>
2019-01-17 15:22:52 +0000

    Add files via upload

---
+++ Functional_Programming/Length.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname Length) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+
+(define lengths
+  (lambda(lst)
+    (cond ((null? lst) 0)
+          (else (+ 1 (length(cdr lst))))
+          )
+    )
+  )
+
+(define lst1 '(1 2 3 4 5 6 7 8 9 (0 0 0)))
+
+(define lst2 '((a b c) 2 3 (4 5 6 7) 5 6))
