a5959c440179e8e5a03e22ed103ef0ee171d3ee7
niallmc14 <niallmc14@gmail.com>
2019-01-21 20:48:03 +0000

    Add files via upload

---
+++ Functional_Programming/Occurences.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname Occurences) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+(define occurences
+  (lambda(lst v)
+    (cond ((null? lst)  0 )
+          ((equal? v (car lst))  (+ 1 (occurences(cdr lst) v))  )
+          (else (occurences(cdr lst) v))
+          )
+    )
+  )
+
+(define lst '(1 2))
+(define lst2 '(1 2 3 4 5 5 5 5 6 5 6 4 3))
